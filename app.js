8747e435651d47f7130fe2cafc05eb4647258ac8
James Patrick Kok <80896379+Diffyyy@users.noreply.github.com>
2024-06-04 10:59:59 +0800

    connect db to app

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const mysql = require('mysql2');
 const path = require('path');
+const port = 3000;
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',     // Replace with your database host
+    user: 'root',          // Replace with your database user
+    password: 'Pass1234!',  // Replace with your database password
+    database: 'mydb'    // Replace with your database name
+});
+
+// Connect to the database
+db.connect(err => {
+    if (err) {
+      console.error('Error connecting to the database:', err.stack);
+      return;
+    }
+    console.log('Connected to the database');
+});
+
+// DISCLAIMER: TRy executing a sql statement
+// const sql = "INSERT INTO user (id, firstname, lastname, email, password, phone, pfp) VALUES ('1', 'A', 'A', 'try@email.com', '1234', '1234', 'lmao')";
+//     db.query(sql, (err, result) => {
+//         if (err) {
+//             console.error('Error executing SQL query:', err.stack);
+//             return;
+//         }
+// });
 
 // Serve static files (CSS, JS, images)
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,7 +53,7 @@ app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'admin.html'));
 });
 
-const port = 3000;
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
