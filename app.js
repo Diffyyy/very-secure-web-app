b33db422c5641db0c0084dc5ab954a3a7d9e92f3
BurgerTruck <jaimeperez526@ymail.com>
2024-06-05 23:43:23 +0800

    hash and salt password, added function for adding user

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const multer = require('multer');
 const fs = require('fs');
 const port = 3000;
 const app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 
@@ -65,7 +67,50 @@ const upload = multer({
     limits: { fileSize: 2 * 1024 * 1024 }  // Limit file size to 2MB
 });
 
+//function for adding user
+const addUser = ({ firstname, lastname, email, number, password, pfp}) => {
+	return new Promise((resolve, reject) => {
+		return db.beginTransaction(err => {
+			if (err) {
+				return reject("Error occurred while creating the transaction");
+			}
+			const query = 'SELECT MAX(id) AS maxId FROM user';
+			//For now, add random number to max id to create new id
+			db.query(query, (error, results, fields) => {
+				if (error) {
+					console.error('Error executing query:', error.stack);
+					return;
+				}
 
+				const maxId = results[0].maxId;
+
+				const min = 1;
+				const max = 100;
+				const randomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+
+				const newId = maxId + randomInt
+				console.log(password)
+				db.execute(
+					'INSERT INTO user (id, firstname, lastname, email, password,phone, pfp) VALUES (?, ?, ?, ?, ?, ?, ?)', [newId, firstname, lastname, email, password, number, pfp], (err) =>{
+						if(err){
+							return db.rollback(()=>{
+								return reject(err)
+							});
+						}
+						return db.commit((err)=>{
+
+							if(err){
+								return db.rollback(()=>{
+									return reject("Commit failed")
+								})
+							}
+
+						});
+					});
+			});
+		});
+	})
+}
 // TODO: Backend checking: need to check again if all fields are valid
 // FIXME: if want a button that goes back to home, need to change this else ok
 app.post('/signup', upload.single('profilepic'), async (req, res) => {
@@ -82,8 +127,7 @@ app.post('/signup', upload.single('profilepic'), async (req, res) => {
         //FIXME: [DOES NOT WORK] return to signup page, restore filled out fields, and indicate an error message
         return res.redirect('/signup');
     }
-
-    try{
+   try{
         const fileType = await import('file-type');
         const fileBuffer = fs.readFileSync(profilepic.path);
         const fileTypeResult = await fileType.fileTypeFromBuffer(fileBuffer);
@@ -95,7 +139,11 @@ app.post('/signup', upload.single('profilepic'), async (req, res) => {
         }
         else{ //Signup success
             //TODO: Put to db
-
+			const hash = bcrypt.hashSync(password,saltRounds)
+			
+			addUser({firstname, lastname, email, number, password: hash, pfp:'PFP_PATH'}).catch(err=>{ //change pfp path here
+				console.log(err)
+			});
             //Back to login
             return res.redirect('/login');
         }
